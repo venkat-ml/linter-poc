b0f5fbf104d92fc5492a03baecb44a10d4fb9efc
Venkat Lanka <durgaprasadlvs@gmail.com>
2022-05-19 16:20:12 +1000

    errors in nodeDetails.groovy

--- nodeDetails.groovy
+++ nodeDetails.groovy
@@ -1,11 +1,17 @@
 import jenkins.model.Jenkins
-def call() {
-    Computer[] computers = Jenkins.get().getComputers()
+
+
+
+def call() 
+    {
+        Computer[] computers = Jenkins.get().getComputers()
     List nodes_offline_list = [];
     computers.each {
-        if (it.isOnline()==false) {
+        if (it.isOnline()==false) 
+        {
             nodes_offline_list.add(it.getDisplayName())
         }
     }
     return nodes_offline_list
+        
 }
